fcb8ef6e6be953d17ef6f634082d53a6a00388e7
Gulshat Galimova <gulshat-galimova@mail.ru>
2023-03-17 13:32:04 +0400

    add Section class

--- scripts/Section.js
+++ scripts/Section.js
@@ -1,7 +1,16 @@
 class Section {
-    constructor({items, renderer}, container) {
+    constructor({items, renderer}, containerSelector) {
         this._items = items;
         this._renderer = renderer;
-        this._container = container;
+        this._containerSelector = containerSelector;
+    }
+    renderItems() {
+        this._items.forEach( item => {
+            const element = renderer(item);
+            this.addItem(element);
+        });
+    }
+    addItem(element) {
+        document.querySelector(this._containerSelector).append(element);
     }
 }
